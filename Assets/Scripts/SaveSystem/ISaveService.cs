d09459bfa8bba6edf64f3206a1b53ada38e5639b
Ömer Kürşat Türköne <38076159+KursatTurkone@users.noreply.github.com>
2025-07-22 15:40:43 +0300

    ISaveService and SaveService seperated

--- Assets/Scripts/SaveSystem/ISaveService.cs
+++ Assets/Scripts/SaveSystem/ISaveService.cs
@@ -3,9 +3,3 @@ public interface ISaveService
     void Save(GameSaveData data, string key);
     GameSaveData Load(string key);
 }
-
-public class SaveService : ISaveService
-{
-    public void Save(GameSaveData data, string key) => SaveSystem.Save(data, key);
-    public GameSaveData Load(string key) => SaveSystem.Load<GameSaveData>(key) ?? new GameSaveData();
-}
